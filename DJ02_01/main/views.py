3ac01dc987fc0efaf9b2411e79c6db777e318e39
Ivan Zyulin <solar.ivz@gmail.com>
2024-11-24 00:08:05 +1200

    upd code

--- DJ02_01/main/views.py
+++ DJ02_01/main/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
-
 def index(request):
-    return HttpResponse("<h1>Это мой первый проект на Django</h1>")
+    return render(request, 'main/index.html')
+def new(request):
+    return render(request, 'main/new.html')
