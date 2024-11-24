cf41b77e8febe679a57a63699697f6d8c3625e07
Ivan Zyulin <solar.ivz@gmail.com>
2024-11-24 23:26:38 +1200

    upd

--- DJ02_01/main/urls.py
+++ DJ02_01/main/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
-from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
 urlpatterns = [
-    path('', views.index),
-    path('new', views.new)
-]
+    path('admin/', admin.site.urls),
+    path('', include('main.urls'))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
